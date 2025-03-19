58701ab4e3c93eaa705b5a3072be539f0ddaa5b0
ritika549 <ritikanairofficial@gmail.com>
2025-03-19 17:36:33 +0530

    created new car class

---
+++ src/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private String name;
+    private String model;
+    private int year;
+
+    public Car(String name, int year, String model) {
+        this.name = name;
+        this.year = year;
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
